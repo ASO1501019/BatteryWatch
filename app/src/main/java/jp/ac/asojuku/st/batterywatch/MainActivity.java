8e543d7458f0fbcb402d8560a3d4d8865027020f
橋口　巧 <Takumi@hashiguchitakumi-no-MacBook-Air.local>
2017-02-02 13:04:58 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/asojuku/st/batterywatch/MainActivity.java
@@ -0,0 +1,136 @@
+package jp.ac.asojuku.st.batterywatch;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private MyBroadcastReciever mReceiver;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mReceiver = new MyBroadcastReciever();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mReceiver, filter);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://jp.ac.asojuku.st.batterywatch/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://jp.ac.asojuku.st.batterywatch/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    public class MyBroadcastReciever extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            //複数のインテントを受信する場合はif文を使う
+            if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
+
+                int scale = intent.getIntExtra("scale", 0);
+                int level = intent.getIntExtra("level", 0);
+                int status = intent.getIntExtra("status", 0);
+                String statusString = "";
+                switch (status) {
+                    case BatteryManager.BATTERY_STATUS_UNKNOWN:
+                        statusString = "unknown";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_CHARGING:
+                        statusString = "charging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_NOT_CHARGING:
+                        statusString = "not charging";
+                        break;
+                    case BatteryManager.BATTERY_STATUS_FULL:
+                        statusString = "full";
+                        break;
+                }
+                final Calendar calendar = Calendar.getInstance();
+                final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                final int minute = calendar.get(Calendar.MINUTE);
+                final int second = calendar.get(Calendar.SECOND);
+
+                String title = "Battery Watch";
+                String msg = "" + hour + ":" + minute + ":" + second + " " + statusString + " " + level + "/" + scale;
+                Log.v(title,msg);
+
+                Activity mainActivity = (Activity) context;
+                TextView tvTitle = (TextView) mainActivity.findViewById(R.id.tvTitle);
+
+                tvTitle.setText(title);
+
+                TextView tvMsg = (TextView) mainActivity.findViewById(R.id.tvMsg);
+
+                tvMsg.setText(msg);
+
+            }
+        }
+    }
+
+    ;
+}
